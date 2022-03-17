3b745dbd41975e7fc274e81b2ef2679bbb4c3d24
christine713 <c890713love@gmail.com>
2022-03-17 06:12:56 +0000

    create post.proto

--- x/weather/keeper/msg_server_weather_post.go
+++ x/weather/keeper/msg_server_weather_post.go
@@ -10,8 +10,15 @@ import (
 func (k msgServer) WeatherPost(goCtx context.Context, msg *types.MsgWeatherPost) (*types.MsgWeatherPostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Create variable of type Post
+	var post = types.Post{
+		Creator: msg.Creator,
+		Title:   msg.Title,
+		Body:    msg.Body,
+	 }
+	// Add a post to the store and get back the ID
+	id := k.AppendPost(ctx, post)
+	// Return the ID of the post
+	return &types.MsgCreatePostResponse{Id: id}, nil
 
-	return &types.MsgWeatherPostResponse{}, nil
 }
